2b37bcb35bb6c1c7abb836bd641c2fa3d8b9c6cf
Shahazad M. Shaikh <shahazads@met.edu>
2026-03-18 11:29:11 +0530

    Added: Form View controller functionality is added

---
+++ DataBoundsControlDemo/FormVIew.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FormVIew.aspx.cs" Inherits="DataBoundsControlDemo.FormVIew" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:FormView ID="FormView1" runat="server"  DefaultMode="ReadOnly" AllowPaging="true" OnPageIndexChanging="FormView1_PageIndexChanging">
+                <ItemTemplate>
+        <table style="border:1px solid #ccc; padding:10px; width:300px; background-color:#f9f9f9;">
+            <tr>
+                <td style="font-weight:bold;">Roll No:</td>
+                <td><%# Eval("RollNo") %></td>
+            </tr>
+            <tr>
+                <td style="font-weight:bold;">Name:</td>
+                <td><%# Eval("Name") %></td>
+            </tr>
+            <tr>
+                <td style="font-weight:bold;">Course:</td>
+                <td><%# Eval("Course") %></td>
+            </tr>
+        </table>
+    </ItemTemplate>
+            </asp:FormView>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ DataBoundsControlDemo/FormVIew.aspx.cs
@@ -0,0 +1,50 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DataBoundsControlDemo
+{
+    public partial class FormVIew : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindDetails();
+            }
+        }
+
+        private void BindDetails()
+        {
+            using (OracleConnection con = new OracleConnection(
+                ConfigurationManager.ConnectionStrings["OracleDBConnectionString"].ConnectionString))
+            {
+                OracleDataAdapter da = new OracleDataAdapter("SELECT * FROM Students", con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                FormView1.DataSource = dt;
+                FormView1.DataBind();
+
+            }
+
+        }
+
+        protected void DetailsView1_PageIndexChanging(object sender, DetailsViewPageEventArgs e)
+        {
+            FormView1.PageIndex = e.NewPageIndex;
+            BindDetails(); // rebind your data source
+        }
+
+        protected void FormView1_PageIndexChanging(object sender, FormViewPageEventArgs e)
+        {
+            FormView1.PageIndex = e.NewPageIndex;
+            BindDetails();
+        }
+    }
+}
